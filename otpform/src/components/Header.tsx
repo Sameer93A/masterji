eae7a26b1a1e10e9eded2dac081b2f6ebefc7759
Sameer Ahmed <sameer937655@gmail.com>
2024-07-25 14:42:19 +0530

    all file in root folder

--- otpform/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-  return (
-    <div className="flex justify-center">
-      <div className="w-[553px] h-[97px] top-[38px] left-[480px] font-inter font-[700] text-[80px] leading-[96.82px] text-white drop-shadow pt-[38px]">
-        Chai aur Code
-      </div>
-    </div>
-  );
-};
